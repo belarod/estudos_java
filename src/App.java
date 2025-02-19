af2b4197d4b3372ff1aea8af019ba938259f10f3
Isabela Rodrigues <bela.rodjj@gmail.com>
2025-02-19 17:59:51 -0300

    Inicialização padrão.#

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) { //main pq é o método principal; void pq não vai retornar nada, e static pq não precisa instanciar a classe
+        System.out.println("Hello, World!");
+    }
+}
